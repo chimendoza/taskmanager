61bdc20a71da831b2d15bbaec22e1528f06651c3
Jairo <chimendozajairo@gmail.com>
2025-12-12 13:19:57 -0600

    Redirección para panel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
+import { routes } from "./src/constants/routes";
 
 const nextConfig: NextConfig = {
   /* config options here */
 
   //reactStrictMode: false
+
+ async redirects() {
+    return [
+      {
+        source: "/",
+        destination: routes.dashboard,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
